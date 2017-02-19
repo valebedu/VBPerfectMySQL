ff8ddb4779374bee4b04d5715a9bc39f6f243806
Valentin Bercot <valent1.bercot@gmail.com>
2017-02-19 17:18:04 +0100

    Document VabeStoreMySQL

--- Sources/VabeStoreMySQL.swift
+++ Sources/VabeStoreMySQL.swift
@@ -9,11 +9,30 @@
 import Foundation
 import MySQL
 
+/**
+ `VabeStoreMySQL` class help a MySQL store class by adding the common logic about initializing and deinitializing the connection to MySQL database. This class should be extended in order to be used properly.
+ 
+ - authors: Valentin Bercot
+ */
 public class VabeStoreMySQL
 {
+    /**
+     MySQL instance, this instance should be used to make MySQL querries.
+     */
     public let mySql: MySQL
+    
+    /**
+     MySQL date formatter, this formatter should be used to convert native datetime to MySQL datetime and vice versa.
+     */
     public let dateFormatter: DateFormatter
     
+    /**
+     - parameters:
+       - host: the MySQL host.
+       - user: the MySQL user name.
+       - password: the MySQL user password.
+       - database: the MySQL database name.
+     */
     public init(host: String, user: String, password: String, database: String) throws
     {
         mySql = MySQL()
@@ -40,6 +59,10 @@ public class VabeStoreMySQL
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
     }
     
+    /**
+     - parameters:
+       -configurator: the MySQL configurator with host, user, password and database.
+     */
     public convenience init(configurator: VabeStoreMySQLConfigurator) throws
     {
         do
